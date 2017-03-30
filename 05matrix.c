de0dadc82577a03ad7a1bbba124515e51f2fdf72
christoph <christoph.abloescher@gmail.com>
2017-03-30 11:54:40 +0200

    function added

--- 05matrix.c
+++ 05matrix.c
@@ -2,59 +2,68 @@
 #include <stdlib.h>
 #include <time.h>
 #include <sys/time.h>
+#include <pthread.h>
+#define N 3
+#define M 3
 
-//void mult();
+
+void mult1();
+void mult2();
+
+int A[N][M];
+int B[N][M];
+int C[N][M];
 
 int main(void)
 {
-	int n = 3;
-	int m = 3;
-	int i, j, k;
-	int A[n][m];
-	int B[n][m];
-	int C[n][m];
+	pthread_t thread1, thread2;
+	int i, j;
 	struct timeval t1, t2;
 	double diff;
 
 	srand(time(NULL));
 	//Matrix A und B befüllen und C leeren
-	for(i = 0; i < n; i++)
+	for(i = 0; i < N; i++)
 	{
-		for(j = 0; j < m; j++)
+		for(j = 0; j < M; j++)
 		{
 			A[i][j]=rand()%10;
 			B[i][j]=rand()%10;
 			C[i][j]=0;
 		}
 	}
-	//mult();
-
-
-
+	
 	gettimeofday(&t1, NULL);
-	for(i = 0; i < n; i++)
+	mult1();
+	mult2();
+	gettimeofday(&t2, NULL);
+	
+
+
+/*	gettimeofday(&t1, NULL);
+	for(i = 0; i < N; i++)
 	{
-		for(k = 0; k < m; k++)
+		for(k = 0; k < M; k++)
 		{		
-			for(j = 0; j < m; j++)
+			for(j = 0; j < M; j++)
 			{
 				C[i][k]=C[i][k]+A[i][j]*B[j][k];
 			}
 		}
 	}
-	gettimeofday(&t2, NULL);
+	gettimeofday(&t2, NULL); */
 	diff = (t2.tv_sec+t2.tv_usec*0.000001)-(t1.tv_sec+t1.tv_usec*0.000001);
 	printf("Diff: %f\n", diff);
 
 ///////////////////////print A, B, C//////////////////////////
-/*	for(i = 0; i < n; i++)
+	for(i = 0; i < N; i++)
 	{
-		for(j = 0; j < m; j++)
+		for(j = 0; j < M; j++)
 		{
 			printf("%d ", A[i][j]);
 		}
 		printf("\t");
-		for(j = 0; j < m; j++)
+		for(j = 0; j < M; j++)
 		{
 			printf("%d ", B[i][j]);
 		}
@@ -63,33 +72,48 @@ int main(void)
 	printf("\n");
 
 
-
-	for(i = 0; i < n; i++)
+	for(i = 0; i < N; i++)
 	{
-		for(j = 0; j < m; j++)
+		for(j = 0; j < M; j++)
 		{
 			printf("%d ", C[i][j]);
 		}
 		printf("\n");
 	}
-*/
+
 	exit(0);
 }
 
-/*
-mult()
+
+
+void mult1()
 {
-	for(i = 0; i < n; i++)
+	int i, j, k;
+	for(i = 0; i < N; i++)
 	{
-		for(k = 0; k < m; k++)
+		for(k = 0; k < (M/2); k++)
 		{		
-			for(j = 0; j < m; j++)
+			for(j = 0; j < M; j++)
 			{
 				C[i][k]=C[i][k]+A[i][j]*B[j][k];
 			}
 		}
 	}
 }
-*/
+
+void mult2()
+{
+	int i, j, k;
+	for(i = 0; i < N; i++)
+	{
+		for(k = (M/2); k < M; k++)
+		{		
+			for(j = 0; j < M; j++)
+			{
+				C[i][k]=C[i][k]+A[i][j]*B[j][k];
+			}
+		}
+	}
+}
 
 
